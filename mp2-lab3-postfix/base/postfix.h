3fd9bdd7608c9a4d2f2b5513a01be8ff436660b4
Tretyakov.A <sabber777rus@gmail.com>
2016-12-28 19:24:30 +0300

    Add files via upload
    done

---
+++ mp2-lab3-postfix/base/postfix.h
@@ -0,0 +1,32 @@
+﻿//-
+#ifndef __POSTFIX_H__
+#define __POSTFIX_H__
+
+#include <string>
+#include "stack.h"
+
+using namespace std;
+
+class TPostfix
+{
+	string infix;
+	string postfix;
+public:
+	TPostfix(string val)
+	{
+		infix = val;
+	}
+
+	int Priority(char val)
+	{
+		if ((val == '*') || (val == '/')) return 2;
+		if ((val == '+') || (val == '-')) return 1;
+		return -7;
+	}
+	string GetInfix() { return infix; }
+	string GetPostfix() { return postfix; }
+	string ToPostfix();
+	double Calculate();
+};
+
+#endif
